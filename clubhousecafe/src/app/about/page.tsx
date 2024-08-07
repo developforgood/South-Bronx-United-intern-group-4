d6000e1cf2e3e4a267afb3fdbab075ad5ac30387
pdainer <pdainer@bloomberg.net>
2024-08-07 16:42:19 -0400

    publishing phoebe branch

---
+++ clubhousecafe/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function about () {
+    return (
+        <>
+        <p>HELLO</p>
+        <p>DARKNESS</p>
+        <p>MY</p>
+        <p>OLD</p>
+        <p>FRIEND</p>
+        </>
+    );
+}
