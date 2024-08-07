81902df0477ef675e8d22a6dbf5c779f4558324c
pdainer <pdainer@bloomberg.net>
2024-08-07 17:47:29 -0400

    phoebe

--- clubhousecafe/src/app/about/page.tsx
+++ clubhousecafe/src/app/about/page.tsx
@@ -1,11 +1,41 @@
+import React, { useLayoutEffect } from "react";
+
+const topbar = {
+   backgroundColor: "white",
+   padding: "35px 0px 35px 0px",
+}
+
+const bottombar = {
+    backgroundColor: "white",
+    padding: "35px 0px 35px 0px",
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+    right: 0,
+ }
+
+
+const absoluteCenter =  {
+    color: "black",
+    alignItems: "center", // does vertically center the desired content
+    justifyContent: "center", // horizontally centers single line items
+    textAlign: "center", // optional, but helps horizontally center text that breaks into multiple lines
+}
+
+
 export default function about () {
     return (
         <>
-        <p>HELLO</p>
-        <p>DARKNESS</p>
-        <p>MY</p>
-        <p>OLD</p>
-        <p>FRIEND</p>
+        <div style={topbar}>
+            <h1 style={absoluteCenter} >About</h1>
+        </div>
+        <div style={{"backgroundColor":"#e1f5ff", "padding": "35px 0px 35px 0px"}}>
+            <h1 >About</h1>
+        </div>
+        <div style={bottombar}>
+            <h1 style={{"color":"black"}}>BOTTON</h1>
+        </div>
+
         </>
     );
 }
